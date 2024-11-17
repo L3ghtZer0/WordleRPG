7eb695e4b8cb2bd239f58fecd2b7e1d81b11ce52
Tebryn05 <158665169+Tebryn05@users.noreply.github.com>
2024-11-16 23:44:25 -0500

    Add files via upload

---
+++ PerformanceReport.cpp
@@ -0,0 +1,45 @@
+#include "PerformanceReport.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void PerformanceReport::displayReport() {
+	ifstream statsFile("wordleStats.txt");
+	if (!statsFile) {
+		cout << "No games have been played yet." << endl;
+		return;
+	}
+
+
+	int totalGames = 0, totalWins = 0, totalTries = 0, score;
+	while (statsFile >> score) {
+		totalGames++;
+		if (score > 0) {
+			totalWins++;
+			totalTries += score;
+		}
+	}
+
+	if (totalGames == 0) {
+		cout << "No games have been played yet." << endl;
+		return;
+	}
+
+	double winPercentage = (double(totalWins) / totalGames) * 100;
+	double averageTries = totalWins > 0 ? (double(totalTries) / totalWins) : 0;
+
+	cout << "Performance Report:" << endl;
+	cout << "Games Played: " << totalGames << endl;
+	cout << "Win Percentage: " << winPercentage << "%" << endl;
+	cout << "Average Tries (for wins): " << averageTries << endl;
+}
+
+void PerformanceReport::saveScore(int score) {
+	ofstream statsFile("wordleStats.txt", ios::app);
+	if (!statsFile) {
+		cerr << "Error: Unable to save stats." << endl;
+		return;
+	}
+	statsFile << score << endl;
+}
