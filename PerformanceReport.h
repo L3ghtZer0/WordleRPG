73ebf1319662b6fc089836207f8c95114c4f206f
De'Marion <150834666+L3ghtZer0@users.noreply.github.com>
2024-11-17 23:48:58 -0500

    Update PerformanceReport.h

--- PerformanceReport.h
+++ PerformanceReport.h
@@ -1,8 +1,9 @@
 #ifndef PERFORMANCEREPORT_H
 #define PERFORMANCEREPORT_H
-
+// Class specification file for PerformanceReport class
 class PerformanceReport {
 	public:
+		// Static functions to ensure encapsulation and mitigate accidental changes
 		static void displayReport();
 		static void saveScore(int score);
 };
