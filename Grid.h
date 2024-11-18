3839fc5e4a9aecbf7e6817b7b46739d3768c3fd9
Tebryn05 <158665169+Tebryn05@users.noreply.github.com>
2024-11-17 23:10:06 -0500

    Update Grid.h

--- Grid.h
+++ Grid.h
@@ -1,11 +1,13 @@
 #ifndef GRID_H
 #define GRID_H
 
+// include string and use std namespace
 #include <string>
 using namespace std;
 
+// define Grid Class
 class Grid {
-	public:
+	public: // function prototypes
 		static void displayFeedback(const string& word, const string& guess);
 		static int stringLength(const string& str);
 		static bool charExistsInString(const string& str, char ch);
