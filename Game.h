dddbbc372353efc600a2fe39eab1516a66c90f09
Tebryn05 <158665169+Tebryn05@users.noreply.github.com>
2024-11-17 23:09:06 -0500

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,14 +1,16 @@
 #ifndef GAME_H
 #define GAME_H
 
+// include string and use std namespace
 #include <string>
 using namespace std;
 
+// define Game Class
 class Game {
-	private:
+	private: // variables
 		string word;
 		int tries;
-	public:
+	public: // function prototypes
 		Game();
 		void rules();
 		string getWord();
